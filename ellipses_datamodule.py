dc706265e5b52af1522dc4f0c155225e050417cb
Alexander Auras <33501357+AlexanderAuras@users.noreply.github.com>
2022-10-11 07:44:23 +0200

    Fixes datamodule

--- ellipses_datamodule.py
+++ ellipses_datamodule.py
@@ -6,8 +6,6 @@ import omegaconf
 import pytorch_lightning as pl
 import torch
 import torch.utils.data
-import torchvision
-import torchvision.transforms as transforms
 import matplotlib.patches
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,31 +19,33 @@ class _EllipsesDataset(torch.utils.data.Dataset):
         self.ellipses_count = ellipses_count
         self.ellipses_size = ellipses_size
         self.ellipses_size_min = ellipses_size_min
-        self.ellipse_width_aa = np.random.rand(img_count)*max(0.0, self.ellipses_size-self.ellipses_size_min)+self.ellipses_size_min
-        self.ellipse_height_aa = np.random.rand(img_count)*max(0.0, self.ellipses_size-self.ellipses_size_min)+self.ellipses_size_min
-        self.ellipse_x_raw = np.random.rand(img_count)
-        self.ellipse_y_raw = np.random.rand(img_count)
-        self.ellipse_angle = np.random.rand(img_count)*360.0
+        self.ellipse_width_aa = torch.rand((img_count*ellipses_count,))*max(0.0, self.ellipses_size-self.ellipses_size_min)+self.ellipses_size_min
+        self.ellipse_height_aa = torch.rand((img_count*ellipses_count,))*max(0.0, self.ellipses_size-self.ellipses_size_min)+self.ellipses_size_min
+        self.ellipse_x_raw = torch.rand((img_count*ellipses_count,))
+        self.ellipse_y_raw = torch.rand((img_count*ellipses_count,))
+        self.ellipse_angle = torch.rand((img_count*ellipses_count,))*360.0
+        self.ellipse_alpha = torch.rand((img_count*ellipses_count,))*0.9+0.1
 
     def __len__(self) -> int:
         return self.img_count
 
-    def __item__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> torch.Tensor:
         fig = plt.figure(figsize=(self.img_size,self.img_size), dpi=1)
         ax = fig.add_axes([0.0,0.0,1.0,1.0])
         ellipse_func = lambda w, h, a, t: (w/2.0*cos(t)*cos(a)-h/2.0*sin(t)*sin(a), w/2.0*cos(t)*sin(a)+h/2.0*sin(t)*cos(a))
-        for _ in range(self.ellipses_count):
-            args = (self.ellipse_width_aa[idx], self.ellipse_height_aa[idx], self.ellipse_angle[idx]/180.0*np.pi)
-            t = atan(-self.ellipse_height_aa[idx]*tan(self.ellipse_angle[idx]/180.0*np.pi)/self.ellipse_width_aa[idx])
-            ellipse_width = max(ellipse_func(*args, t)[0], ellipse_func(*args, t+np.pi)[0])-min(ellipse_func(*args, t)[0], ellipse_func(*args, t+np.pi)[0])
-            t = atan(self.ellipse_height_aa[idx]/(tan(self.ellipse_angle[idx]/180.0*np.pi)*self.ellipse_width_aa[idx]))
-            ellipse_height = max(ellipse_func(*args, t)[1], ellipse_func(*args, t+np.pi)[1])-min(ellipse_func(*args, t)[1], ellipse_func(*args, t+np.pi)[1])
-            ellipse_x = ellipse_width/2.0+self.ellipse_x_raw[idx]*(self.img_size-ellipse_width)
-            ellipse_y = ellipse_height/2.0+self.ellipse_y_raw[idx]*(self.img_size-ellipse_height)
-            ellipse = matplotlib.patches.Ellipse(xy=[ellipse_x, ellipse_y], width=self.ellipse_width_aa[idx], height=self.ellipse_height_aa[idx], angle=self.ellipse_angle[idx])
+        for i in range(self.ellipses_count):
+            e_idx = idx*self.ellipses_count+i
+            args = (self.ellipse_width_aa[e_idx].item(), self.ellipse_height_aa[e_idx].item(), self.ellipse_angle[e_idx].item()/180.0*torch.pi)
+            t = atan(-self.ellipse_height_aa[e_idx].item()*tan(self.ellipse_angle[e_idx].item()/180.0*torch.pi)/self.ellipse_width_aa[e_idx].item())
+            ellipse_width = max(ellipse_func(*args, t)[0], ellipse_func(*args, t+torch.pi)[0])-min(ellipse_func(*args, t)[0], ellipse_func(*args, t+torch.pi)[0])
+            t = atan(self.ellipse_height_aa[e_idx].item()/(tan(self.ellipse_angle[e_idx].item()/180.0*torch.pi)*self.ellipse_width_aa[e_idx].item()))
+            ellipse_height = max(ellipse_func(*args, t)[1], ellipse_func(*args, t+torch.pi)[1])-min(ellipse_func(*args, t)[1], ellipse_func(*args, t+torch.pi)[1])
+            ellipse_x = ellipse_width/2.0+self.ellipse_x_raw[e_idx].item()*(self.img_size-ellipse_width)
+            ellipse_y = ellipse_height/2.0+self.ellipse_y_raw[e_idx].item()*(self.img_size-ellipse_height)
+            ellipse = matplotlib.patches.Ellipse(xy=[ellipse_x, ellipse_y], width=self.ellipse_width_aa[e_idx].item(), height=self.ellipse_height_aa[e_idx].item(), angle=self.ellipse_angle[e_idx].item())
             ax.add_artist(ellipse)
             ellipse.set_clip_box(ax.bbox)
-            ellipse.set_alpha(0.1+0.9*np.random.rand())
+            ellipse.set_alpha(self.ellipse_alpha[e_idx].item())
         ax.axis("off")
         ax.set_xlim(0.0, self.img_size)
         ax.set_ylim(0.0, self.img_size)
@@ -63,14 +63,13 @@ class EllipsesDataModule(pl.LightningDataModule):
         self.config = config
 
     def train_dataloader(self) -> torch.utils.data.DataLoader:
-        training_dataset = _EllipsesDataset((640 if self.training_batch_count == -1 else self.training_batch_count)*self.training_batch_size, self.config.img_size, 100, 5, 5)
-        _, training_dataset = torch.utils.data.random_split(training_dataset, [int(len(training_dataset)*self.config.validation_split_percent/100.0), len(training_dataset)-int(len(training_dataset)*self.config.validation_split_percent/100.0)])
+        training_dataset = _EllipsesDataset((640 if self.config.training_batch_count == -1 else self.config.training_batch_count)*self.config.training_batch_size, self.config.img_size, self.config.ellipse_count, self.config.ellipse_size, self.config.ellipse_size_min)
         return torch.utils.data.DataLoader(training_dataset, drop_last=self.config.drop_last_training_batch, batch_size=self.config.training_batch_size, shuffle=self.config.shuffle_training_data, num_workers=self.config.num_workers)
     
     def val_dataloader(self) -> torch.utils.data.DataLoader:
-        validation_dataset = _EllipsesDataset((160 if self.validation_batch_count == -1 else self.validation_batch_count)*self.validation_batch_size, self.config.img_size, 100, 5, 5)
+        validation_dataset = _EllipsesDataset((160 if self.config.validation_batch_count == -1 else self.config.validation_batch_count)*self.config.validation_batch_size, self.config.img_size, self.config.ellipse_count, self.config.ellipse_size, self.config.ellipse_size_min)
         return torch.utils.data.DataLoader(validation_dataset, drop_last=self.config.drop_last_validation_batch, batch_size=self.config.validation_batch_size, shuffle=self.config.shuffle_validation_data, num_workers=self.config.num_workers)
 
     def test_dataloader(self) -> torch.utils.data.DataLoader:
-        test_dataset = _EllipsesDataset((200 if self.test_batch_count == -1 else self.test_batch_count)*self.test_batch_size, self.config.img_size, 100, 5, 5)
+        test_dataset = _EllipsesDataset((200 if self.config.test_batch_count == -1 else self.config.test_batch_count)*self.config.test_batch_size, self.config.img_size, self.config.ellipse_count, self.config.ellipse_size, self.config.ellipse_size_min)
         return torch.utils.data.DataLoader(test_dataset, drop_last=self.config.drop_last_test_batch, batch_size=self.config.test_batch_size, shuffle=self.config.shuffle_test_data, num_workers=self.config.num_workers)
